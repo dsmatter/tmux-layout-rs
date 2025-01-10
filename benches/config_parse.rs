fdaab03cb03d7ec824ec3a74fee4ceea7b46d58c
Daniel Strittmatter <github@smattr.de>
2025-01-10 10:21:06 +0100

    fix: benchmark tests

--- benches/config_parse.rs
+++ benches/config_parse.rs
@@ -7,7 +7,9 @@ fn criterion_benchmark(c: &mut Criterion) {
     let config_bytes_yml = include_bytes!("../examples/config/.tmux-layout.yml");
 
     c.bench_function("build_command", |b| {
-        let config = toml::from_slice::<Config>(config_bytes_toml).unwrap();
+        let config_str_toml = std::str::from_utf8(config_bytes_toml).unwrap();
+        let config = toml::from_str::<Config>(config_str_toml).unwrap();
+
         b.iter(|| {
             TmuxCommandBuilder::new("tmux", std::iter::empty::<String>())
                 .new_sessions(&config.sessions)
@@ -21,7 +23,8 @@ fn criterion_benchmark(c: &mut Criterion) {
     });
     c.bench_function("parse_config_toml", |b| {
         b.iter(|| {
-            toml::from_slice::<Config>(black_box(config_bytes_toml)).unwrap();
+            let config_str_toml = std::str::from_utf8(config_bytes_toml).unwrap();
+            toml::from_str::<Config>(black_box(config_str_toml)).unwrap();
         })
     });
 }
